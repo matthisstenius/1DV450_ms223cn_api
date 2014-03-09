bd219a75f93ac3fd7200fd54a156ed85a4915e6e
Matthis Stenius <matthis@me.com>
2014-03-09 17:45:15 +0100

    fix

--- app/controllers/api/v1/resources_controller.rb
+++ app/controllers/api/v1/resources_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::ResourcesController < ApplicationController
 	before_action :authorize, only: [:destroy, :update, :create]
 	before_action :check_params, only: [:create, :update]
 	respond_to :json, :xml
-	rescue_from Exception, :with => :handle_exception
+	#rescue_from Exception, :with => :handle_exception
 
 	def index
 		limit = params[:limit] || 25
