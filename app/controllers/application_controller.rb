c57d686ed1ae730381f4e6a341499ede57885e27
Matthis Stenius <matthis@me.com>
2014-03-09 18:01:12 +0100

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -129,10 +129,13 @@ class ApplicationController < ActionController::Base
   private 
 
   def cors_preflight_check
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Authorization, X-Access-Token, Content-Type'
-    headers['Access-Control-Max-Age'] = '1728000'
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS, PUT, DELETE'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Authorization, X-Access-Token, Content-Type'
+      headers['Access-Control-Max-Age'] = '1728000'
+      render :text => '', :content_type => 'text/plain'
+    end
   end
 
   def cors_set_access_control_headers
